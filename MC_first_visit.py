9692cf8ad0ca5d8856534098b284b4c9a70ac513
manish181192 <manishvidyasagar@gmail.com>
2017-02-18 12:41:20 +0530

    environment black jack - basic blackjack (incorrect) MC_first_visit = Sample retreival complete. @To-Do Discounted Reward calculation state value calculation OR Expectancy calculation

--- MC_first_visit.py
+++ MC_first_visit.py
@@ -1,42 +1,28 @@
-from Environment_BLACK_JACK import ACTION,black_jack
+from env_blackjack import BlackjackEnv
+from enum import Enum
+
+bj = BlackjackEnv()
+
+class ACTION(Enum):
+    STICK = 0
+    TWIST = 1
 
 def policy(state):
-    if state[0] < 12:
-        return ACTION.TWIST
-    else:
+    score, dealer_score, usable_ace = state
+    if score >= 20:
         return ACTION.STICK
+    else:
+        return ACTION.TWIST
 
-bj = black_jack()
-no_of_episodes = 500
-discount_factor = 0.9
-state_value = {}
-
+no_of_episodes = 10
+TIME_STEP_LIMIT = 50
 for i in range(no_of_episodes):
-    ########### SAMPLE EPISODES #################
-    time_steps = 0
-    state_list = []
-    action_list = []
-    reward_list = []
-
-    while 1:
-        if time_steps == 0:
-            state_list[0] = bj.deal_hands(no_of_players=1)  # initial state
-            action_list[0] = policy(state_list[0])
-            time_steps = time_steps + 1
-        else:
-            current_reward, next_state, isTerminate = bj.get_next(action= action_list[time_steps-1], state= state_list[time_steps-1])
-            reward_list[time_steps-1] = current_reward
-
-
-
-            if isTerminate == False:
-                state_list[time_steps] = next_state
-                action_list[time_steps] = policy(next_state)
-            else:
+    state_ = bj.reset()
+    for time_steps in range(TIME_STEP_LIMIT):
+            print(state_)
+            action = policy(state_)
+            print(action)
+            state, reward, isTerminate, _ = bj.step( action= action)
+            print(reward)
+            if isTerminate:
                 break
-            time_steps = time_steps + 1
-
-        # COMPUTE Gt (Discounted Reward for every time)
-
-
-
