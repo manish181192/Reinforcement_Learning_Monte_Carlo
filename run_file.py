2dfcd8fed35e3461d822b19110385880ad86b601
manish181192 <manishvidyasagar@gmail.com>
2017-02-23 20:43:08 +0530

    Policy evaluation and improvement
    Policy Evaluation and improvement added

--- run_file.py
+++ run_file.py
@@ -4,6 +4,7 @@ from env_blackjack import BlackjackEnv
 blackJack = BlackjackEnv()
 action_space = blackJack.action_space
 
+######## POLICY EVALUATION #######
 def my_policy(state):
     score, dealer_score, usable_ace = state
     if score >= 19:
@@ -11,8 +12,12 @@ def my_policy(state):
     else:
         return 1
 
-monte_carlo = monte_carlo(alpha=0.02, discount= 0.9, environment= blackJack, action_space= action_space, policy= my_policy)
+monte_carlo_evaluation = monte_carlo(alpha=0.02, discount= 0.9, environment= blackJack, action_space= action_space, policy= my_policy)
 
-monte_carlo.display_state_values()
-# monte_carlo.display_policy()
-monte_carlo.plot_value_graph()
+monte_carlo_evaluation.display_state_values()
+monte_carlo_evaluation.plot_value_graph()
+
+######## POLICY IMPROVEMENT #######
+mc_optimal_policy = monte_carlo(alpha=0.02, discount= 0.9, environment= blackJack, action_space= action_space)
+mc_optimal_policy.display_policy()
+mc_optimal_policy.plot_action_graph()
